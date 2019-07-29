6b80b33a76c57b47c61410ed8690389febf48f59
John Hayden <jthayden10@me.com>
2019-07-29 11:49:40 -0400

    styling

--- client/src/components/routes/SingleRoute.js
+++ client/src/components/routes/SingleRoute.js
@@ -67,8 +67,8 @@ export default class SingleRoute extends Component {
         <h2>{this.state.route.name} </h2>
         <img className="route-image"src={this.state.route.image} />
         <p className='non-card'>{this.state.route.description} </p>
-        <p>Distance:{this.state.route.distance} miles </p>
-        <p>Rating:{this.state.route.rating} stars </p>
+        <p>Distance: {this.state.route.distance} miles </p>
+        <p>Rating: {this.state.route.rating} stars </p>
         <iframe className="route-map" src={this.state.route.routeLink} />
       </div>
     );
